6e3a07e09f7296f1d528ed1d368c05e2fd58ab6b
Paulo Henrique <106350565+VoidPHF@users.noreply.github.com>
2023-03-07 22:30:06 -0300

    Aprendendo o básico de python
    print, bool e afins

--- Aula2/Exe01.py
+++ Aula2/Exe01.py
@@ -1,3 +1,3 @@
-# Sep é usado como separador, end uma alternativa de quebra de linha
-print('Luiz', 'Miguel', sep='-', end='=')
+# Sep é usado como separador, end uma alternativa de quebra de linha. Mas ao digitar /r/n
+print('Luiz', 'Miguel', sep='-', end='= \r\n')
 print('Paulo', 'Henrique', sep='--', end='=')
