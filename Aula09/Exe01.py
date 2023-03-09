0edd0b2fc2df9ce3c4c99e2ef6b0540c83441ad9
Paulo Henrique <106350565+VoidPHF@users.noreply.github.com>
2023-03-09 17:56:00 -0300

    Atualização

--- Aula09/Exe01.py
+++ Aula09/Exe01.py
@@ -7,7 +7,13 @@ imc = peso / (altura * altura)
 
 print(nome, 'tem', altura, 'de altura, ' 'pesa', peso,
       'quilos e tem seu IMC de', imc, sep=' ', end=' ')
-
-linha_1 = f'{nome} tem {altura:.1f} de altura'
-print(linha_1)
 # ... isso se chama elipsia, e funciona como placeholder, assim o codigo não da erro
+
+# Introdução breve a Fstring
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'pesa {peso} quilos e seu imc é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
--- Aula8/Exe01.py
+++ Aula8/Exe01.py
@@ -6,9 +6,3 @@
 conta_1 = (1 + int(0.5 + 0.5)) ** (5 + 5)
 print(conta_1)
 
-nome = 'Paulo Henrique'
-altura = 1.80
-peso = = 70
-imc = peso / (altura * altura)
-
-print(nome 'tem' altura 'de altura, pesa' peso 'e seu IMC é de' imc)
