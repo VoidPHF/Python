df3b746738f09a3d7b9ddb678671689cfb14a3d9
VoidPHF <PHenrique.F.Lima@hotmail.com>
2023-11-27 17:22:33 -0300

    Atualizando python

---
+++ Aula35/Exe01.py
@@ -0,0 +1,11 @@
+vendedores=['maria', 'joão', 'cleber', 'pedro']
+vendas=[40, 23, 35, 62]
+
+for i, vendedor in enumerate(vendedores):
+        print(vendedor)
+        print(vendas[i])
+
+
+for c in range(1, 6): 
+        print("CONTANDO:" )
+        print (c)
